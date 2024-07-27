12d7e474dba31991ae1109d82078baf3b525d6e8
Shivang-25 <22103275@mail.jiit.ac.in>
2024-07-27 09:53:51 +0530

    first commit - integer average

---
+++ Q2_OSS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter size of Array: " << endl;
+    cin >> n;
+
+    int arr[n];
+    double avg;
+    cout << "Enter Numbers: " << endl;
+    for(int i=0;i<n;i++) {
+        cin >> arr[i];
+        avg += arr[i];
+    }
+    avg = avg / n;
+    cout << "Average: " << avg << endl;
+    return 0;
+}
